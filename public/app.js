c4e4beae20443107f00d5ec276c84b36dbacc502
Brad Dwyer <github@barbdwyer.com>
2014-05-13 12:06:47 -0500

    Adding firebase functionality

--- public/app.js
+++ public/app.js
@@ -1,17 +1,19 @@
 var $me, $playpen, $status;
 
+var firebase_root;
+
 $(function() {
+	firebase_root = new Firebase('https://playpen.firebaseio.com');
+
 	FastClick.attach(document.body);
 
 	$playpen = $('#playpen');
 	$status = $('#status');
-	$me = $('<div class="player" />');
 
 	var color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-	$me.css('background-color', color);
 	$status.css('color', color);
 
-	$playpen.append($me);
+	var uid = Math.floor(Math.random()*100000);
 
 	var offset = $playpen.offset();
 	$playpen.click(function(e) {
@@ -24,13 +26,44 @@ $(function() {
 		if(y < 0) y = 0;
 		if(y > 500) y = 500;
 
-		$me.transition({
+		firebase_root.child(uid).set({
+			color: color,
 			x: x,
 			y: y
 		});
 	});
 
+	firebase_root.child(uid).onDisconnect().set(null);
+
 	$('body').on('touchmove', function() {
 		return false;
+	});
+
+	firebase_root.on('child_added', function(snapshot) {
+		var val = snapshot.val();
+
+		var player = $('<div class="player" />').attr('id', 'player_' + snapshot.name());
+		player.css('background-color', val.color);
+		$playpen.append(player);
+
+		player.transition({
+			x: val.x,
+			y: val.y
+		});
+	});
+
+	firebase_root.on('child_removed', function(snapshot) {
+		// remove an element
+		$('#player_' + snapshot.name()).remove();
+	});
+
+	firebase_root.on('child_changed', function(snapshot) {
+		// update an element
+		var val = snapshot.val();
+
+		$('#player_' + snapshot.name()).transition({
+			x: val.x,
+			y: val.y
+		});
 	});
 });
