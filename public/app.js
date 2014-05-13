642e92d5e1ba53211fec85d35d3b0ddfc620fdd5
Brad Dwyer <github@barbdwyer.com>
2014-05-13 12:31:47 -0500

    Adding authentication and security rules

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 var $me, $playpen, $status;
 
-var firebase_root;
+var firebase_root, auth;
 
 $(function() {
 	firebase_root = new Firebase('https://playpen.firebaseio.com');
@@ -10,60 +10,72 @@ $(function() {
 	$playpen = $('#playpen');
 	$status = $('#status');
 
-	var color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-	$status.css('color', color);
+	auth = new FirebaseSimpleLogin(firebase_root, function(error, user) {
+		console.log(user);
+		if(user) {
+			// get color and call setup
+			var uid = user.id;
+			firebase_root.child(uid).once('value', function(snapshot) {
+				var u = snapshot.val();
 
-	var uid = Math.floor(Math.random()*100000);
+				var color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+				if(u && u.color) color = u.color;
 
-	var offset = $playpen.offset();
-	$playpen.click(function(e) {
-		var x = e.clientX - offset.left - 8;
-		var y = e.clientY - offset.top - 8;
+				$status.css('color', color);
 
-		if(x < 0) x = 0;
-		if(x > 500) x = 500;
+				var offset = $playpen.offset();
+				$playpen.click(function(e) {
+					var x = e.clientX - offset.left - 8;
+					var y = e.clientY - offset.top - 8;
 
-		if(y < 0) y = 0;
-		if(y > 500) y = 500;
+					if(x < 0) x = 0;
+					if(x > 500) x = 500;
 
-		firebase_root.child(uid).set({
-			color: color,
-			x: x,
-			y: y
-		});
-	});
+					if(y < 0) y = 0;
+					if(y > 500) y = 500;
 
-	firebase_root.child(uid).onDisconnect().set(null);
+					firebase_root.child(uid).set({
+						color: color,
+						x: x,
+						y: y
+					});
+				});
 
-	$('body').on('touchmove', function() {
-		return false;
-	});
+				$('body').on('touchmove', function() {
+					return false;
+				});
 
-	firebase_root.on('child_added', function(snapshot) {
-		var val = snapshot.val();
+				firebase_root.on('child_added', function(snapshot) {
+					var val = snapshot.val();
 
-		var player = $('<div class="player" />').attr('id', 'player_' + snapshot.name());
-		player.css('background-color', val.color);
-		$playpen.append(player);
+					var player = $('<div class="player" />').attr('id', 'player_' + snapshot.name());
+					player.css('background-color', val.color);
+					$playpen.append(player);
 
-		player.transition({
-			x: val.x,
-			y: val.y
-		});
-	});
+					player.transition({
+						x: val.x,
+						y: val.y
+					});
+				});
 
-	firebase_root.on('child_removed', function(snapshot) {
-		// remove an element
-		$('#player_' + snapshot.name()).remove();
-	});
+				firebase_root.on('child_removed', function(snapshot) {
+					// remove an element
+					$('#player_' + snapshot.name()).remove();
+				});
 
-	firebase_root.on('child_changed', function(snapshot) {
-		// update an element
-		var val = snapshot.val();
+				firebase_root.on('child_changed', function(snapshot) {
+					// update an element
+					var val = snapshot.val();
 
-		$('#player_' + snapshot.name()).transition({
-			x: val.x,
-			y: val.y
-		});
+					$('#player_' + snapshot.name()).transition({
+						x: val.x,
+						y: val.y
+					});
+				});
+			});
+		} else {
+			// login
+			auth.login('anonymous');
+		}
 	});
 });
